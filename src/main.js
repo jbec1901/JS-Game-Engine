f8aabd3e4c27463014ad482327ba9b6dde87e50c
jbec1901 <jbec1901@gmail.com>
2018-09-07 22:06:58 -0500

    re-orginized

---
+++ src/main.js
@@ -0,0 +1,92 @@
+function setup(){
+
+	var canvas = document.getElementById('canvas');
+
+	ctx = canvas.getContext('2d');
+
+	function resizeCanvas(){
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+	}
+	window.addEventListener('resize', resizeCanvas, false);
+	resizeCanvas()
+
+	//All keyboard events
+	//Keydown
+	function keyDown(event) {
+		var key = event.key;
+	};
+	window.addEventListener("keydown", keyDown);
+	//Keyup
+	function keyUp(event) {
+		var key = event.key;
+	};
+	window.addEventListener("keyup", keyUp);
+
+	//All mouse events
+	//Moves moves
+	function mouseMove(event){
+		mouseX = event.x;
+		mouseY = event.y;
+	}
+	window.addEventListener("mousemove", mouseMove);
+	//Mouse Down
+	function mouseDown(event){
+			switch(event.which){
+				case 1:
+					//Left Click
+					break;
+				case 2:
+					//Mid Click
+					break;
+				case 3:
+					//Right Click
+					break;
+			}
+	}
+	window.addEventListener("mousedown", mouseDown);
+	//Mouse Up
+	function mouseUp(event){
+		switch(event.which){
+			case 1:
+				//Left Click
+				break;
+			case 2:
+				//Mid Click
+				break;
+			case 3:
+				//Right Click
+				break;
+		}
+	}
+	window.addEventListener("mouseup", mouseUp);
+	//Mouse scroles
+	function mouseScroll(event){
+		Delta = event.wheelDelta;
+		DeltaY = event.wheelDeltaY;
+		DeltaX = event.wheelDeltaX;
+	}
+	window.addEventListener("mousewheel", mouseScroll,{passive: true});
+
+	now = performance.now();
+	setTimeout(tick,0);
+
+	window.requestAnimationFrame(draw);
+}
+
+function tick(){
+	var deltaTime = performance.now() - now;
+	now += deltaTime;
+
+	console.log(deltaTime);
+
+	setTimeout(tick,0);
+}
+
+function draw(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	window.requestAnimationFrame(draw);
+}
+
+window.onload = setup();
