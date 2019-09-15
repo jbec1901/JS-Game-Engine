d44e421feb54452dff94d8d1951facd6f0a134e7
Leyla <leyla@pop-os.localdomain>
2019-09-14 23:26:02 -0500

    Added Tick and exists to entity

--- src/physics/entity.js
+++ src/physics/entity.js
@@ -1,9 +1,10 @@
 const Vector = require('./vector');
 
 class Entity {
-  constructor(spriteSheet, bounds){
+  constructor(spriteSheet, bounds, tick){
     this.spriteSheet = spriteSheet;
     this.bounds = bounds;
+    this.tick = tick;
   }
 
   createInstence(location){
@@ -19,10 +20,16 @@ class Instence {
 
     this.animation = 0;
     this.frame = 0;
+
+    this.exist = true;
   }
 
   draw(ctx, scale){
     this.parent.spriteSheet.drawImage(ctx, animation, frame, location, scale);
+  }
+
+  tick(){
+    this.parent.tick.bind(this)();
   }
 
   getBounds(){
