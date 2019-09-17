485aa10b0b8b2c683fd642a991eb7497a44d5d41
Leyla <leyla@pop-os.localdomain>
2019-09-17 14:08:06 -0500

    Added defualt game to entitys

--- src/physics/entity.js
+++ src/physics/entity.js
@@ -1,11 +1,35 @@
 const Vector = require('./vector');
 const uID = require('./../logic/uID');
 
+let entityTypes = [];
+let entitys = [];
+
+//Default Game
+const Game = new (require('./../logic/game'))(30, 60);
+//Default Renderer
+Game.renderer.newLayer(function(){
+  this.clear();
+  entitys.map((entity) => {
+    entity.draw(this.ctx);
+  });
+});
+Game.start(() => {
+  entitys.map((entity) => {
+    entity.tick();
+  });
+});
+//Default collision space
+const collisionSpace = new (require('./collision/pool'))();
+
 class Entity {
-  constructor(spriteSheet, bounds, tick){
+  constructor(spriteSheet, bounds, tick, options = {
+
+  }){
     this.spriteSheet = spriteSheet;
     this.bounds = bounds;
     this.tick = tick;
+
+    entityTypes.push(this);
   }
 
   createInstence(location){
@@ -29,10 +53,11 @@ class Instence {
     .then((id) => {
       this.id = id;
     });
+    entitys.push(this);
   }
 
   draw(ctx, scale){
-    this.parent.spriteSheet.drawImage(ctx, animation, frame, location, scale);
+    this.parent.spriteSheet.drawImage(ctx, this.animation, this.frame, this.location);
   }
 
   tick(){
