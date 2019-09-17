b9c95f4247bfce3eca252cb759e463185284bb87
Leyla <leyla@pop-os.localdomain>
2019-09-17 14:05:42 -0500

    Fixed name error

--- src/IO/spriteSheet.js
+++ src/IO/spriteSheet.js
@@ -1,17 +1,17 @@
 class SpriteSheet{
-  constructor(image, columns, rows){
+  constructor(image, width, height){
     this.sheet = new Image;
     this.sheet.src = image;
 
-    this.width = Math.floor(this.sheet.width / columns);
-    this.height = Math.floor(this.sheet.height / rows);
+    this.width = width;
+    this.height = height;
 
   }
 
   drawImage(ctx, row, column, location, scale = 1){
-    let { width, height } = this;
-    let { x, y } = location
-    ctx.drawImage(this.image, width * row, height * column, width, height, x, y, width * scale, height * scale);
+    let { sheet, width, height } = this;
+    let { x, y } = location;
+    ctx.drawImage(sheet, width * row, height * column, width, height, x, y, width * scale, height * scale);
   }
 }
 
