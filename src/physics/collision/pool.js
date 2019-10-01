67272967f5361c6aba6666da8a17f3ad2aa4cbe8
Leyla <leyla@pop-os.localdomain>
2019-10-01 13:19:43 -0500

    Added test collitions to pool

--- src/physics/collision/pool.js
+++ src/physics/collision/pool.js
@@ -27,6 +27,28 @@ class Pool {
     return false;
   }
 
+  //testCollisions is a broader form for testCollision
+  //It will test a collition against every other object and return an array of
+  //all collided objects
+  testCollisions(object){
+    let collitions = [];
+    let bounds = object.getBounds();
+    for(let i = this.objects.length - 1; i >= 0; i--){
+      //If the object dosn't exist anymore remove it from the pool
+      if(!this.objects[i].exist){
+        this.objects.splice(i, 1);
+        continue;
+      }
+      if(object.id === this.objects[i].id){
+        continue;
+      }
+      if(this.objects[i].getBounds().testCollision(bounds)){
+        collitions.push(this.objects[i]);
+      }
+    }
+    return collitions;
+  }
+
   draw(ctx, color){
     this.objects.map((object) => {
       object.getBounds().draw(ctx,color);
