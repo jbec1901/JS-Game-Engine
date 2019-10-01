c79da83842c0d10d93f5055f253994a200f936fb
Leyla <leyla@pop-os.localdomain>
2019-10-01 15:21:04 -0500

    created demo

--- src/main.js
+++ src/main.js
@@ -1,43 +1,72 @@
-const Game = new (require('./logic/game'))(30, 60);
-const SpriteSheet = require('./IO/spriteSheet');
 const Vector = require('./physics/vector');
-const Entity = require('./physics/entity');
+const SpriteSheet = require('./IO/spriteSheet');
+const Entity = require('./physics/entity').Entity;
+const Game = require('./physics/entity').Game;
 const AABB = require('./physics/collision/AABB');
+const UserInput = require('./IO/userInput');
 
-let bb = new AABB(new Vector(10, 10));
-
-// Game.renderer.newStaticLayer(function(){
-//   this.ctx.fillStyle = 'red';
-//   this.ctx.fillRect(20, 20, 100, 100);
-// });
+//Default Renderer
 Game.renderer.newLayer(function(){
-  bb.apply(new Vector(50, 50)).draw(this.ctx, '#ff0f0f');
+  this.clear();
+  Game.mapEntitys((entity) => {
+    entity.draw(this.ctx);
+  });
 });
-// Game.renderer.newLayer(function(){
-//   this.ctx.fillStyle = 'green';
-//   this.ctx.fillRect(10, 10, 100, 100);
-// });
-// Game.start();
 
-// 	now = performance.now();
-// 	setTimeout(tick,0);
-//
-// 	window.requestAnimationFrame(draw);
-// }
-//
-// function tick(){
-// 	var deltaTime = performance.now() - now;
-// 	now += deltaTime;
-//
-// 	console.log(deltaTime);
-//
-// 	setTimeout(tick,0);
-// }
-//
-// function draw(){
-// 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-//
-// 	window.requestAnimationFrame(draw);
-// }
-//
-// window.onload = setup();
+Game.start();
+
+let player = new Entity({
+  spriteSheet: new SpriteSheet('./../res/red.png', 16, 16),
+  bounds: new AABB(new Vector(16)),
+  tick: function(delta){
+    this.location.join(this.velocity.multiply(new Vector(delta / 5)));
+  },
+  triggers: {
+    collision: () => {
+      console.log('hit');
+    },
+  },
+  vars:{
+    velocity: new Vector()
+  }
+}).createInstence(new Vector(0,200));
+
+let other = new Entity({
+  spriteSheet: new SpriteSheet('./../res/green.png', 16, 16),
+  bounds: new AABB(new Vector(16)),
+});
+for(let i = 1; i < 11; i++){
+  other.createInstence(new Vector(i * 80, 200));
+}
+
+UserInput.Controler.addBinding('KeyW', 'up');
+UserInput.Controler.down('up', () => {
+  player.velocity = new Vector(player.velocity.x, -1);
+});
+UserInput.Controler.up('up', () => {
+  player.velocity = new Vector(player.velocity.x, 0);
+});
+
+UserInput.Controler.addBinding('KeyA', 'left');
+UserInput.Controler.down('left', () => {
+  player.velocity = new Vector(-1, player.velocity.y);
+});
+UserInput.Controler.up('left', () => {
+  player.velocity = new Vector(0, player.velocity.y);
+});
+
+UserInput.Controler.addBinding('KeyS', 'down');
+UserInput.Controler.down('down', () => {
+  player.velocity = new Vector(player.velocity.x, 1);
+});
+UserInput.Controler.up('down', () => {
+  player.velocity = new Vector(player.velocity.x, 0);
+});
+
+UserInput.Controler.addBinding('KeyD', 'right');
+UserInput.Controler.down('right', () => {
+  player.velocity = new Vector(1, player.velocity.y);
+});
+UserInput.Controler.up('right', () => {
+  player.velocity = new Vector(0, player.velocity.y);
+});
