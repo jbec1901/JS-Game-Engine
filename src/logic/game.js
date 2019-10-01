6034078aa5f759c8139e6881986b1f03c8061632
Leyla <leyla@pop-os.localdomain>
2019-10-01 15:18:01 -0500

    Moved game stuff out of entity and into Game

--- src/logic/game.js
+++ src/logic/game.js
@@ -7,9 +7,14 @@ class Game {
     this.renderer = new Renderer(fps);
 
     this.tps = tps;
-    this.thread = new Thread(() => {
-
+    this.thread = new Thread((delta) => {
+      this.mapEntitys((entity) => {
+        entity.tick(delta);
+      });
     }, this.tps);
+
+    this.entityTypes = [];
+    this.collisionSpace = new (require('./../physics/collision/pool'))();
   }
 
   start(gameLoop){
@@ -21,10 +26,38 @@ class Game {
       console.log(err);
     }
     if(gameLoop){
-      this.thread = new Thread(gameLoop, this.tps);
+      this.thread = new Thread((delta) => {
+        gameLoop(delta);
+        this.mapEntitys((entity) => {
+          entity.tick(delta);
+        });
+      }, this.tps);
     }
     this.thread.start();
     this.renderer.start();
+  }
+
+  stop(){
+    try {
+      this.renderer.stop();
+      this.thread.stop();
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+
+  mapEntitys(callback){
+    this.entityTypes.map((type) => {
+      for(let i = type.instences.length - 1; i >= 0; i--){
+        let entity = type.instences[i];
+        if(!entity.exist){
+          type.instences.splice(i);
+          return;
+        }
+        callback(entity, type.id);
+      }
+    });
   }
 }
 
--- src/physics/entity.js
+++ src/physics/entity.js
@@ -5,37 +5,8 @@ let entityTypes = [];
 let entitys = [];
 
 //Default Game
-const Game = new (require('./../logic/game'))(0, 60);
-
-//Default Renderer
-Game.renderer.newLayer(function(){
-  this.clear();
-  mapEntitys((entity) => {
-    entity.draw(this.ctx);
-  });
-});
-//Default game loop
-Game.start((delta) => {
-  mapEntitys((entity) => {
-    entity.tick(delta);
-  });
-});
-
-//Default collision space
-const collisionSpace = new (require('./collision/pool'))();
-
-function mapEntitys(callback){
-  entityTypes.map((type) => {
-    for(let i = type.instences.length - 1; i >= 0; i--){
-      let entity = type.instences[i];
-      if(!entity.exist){
-        type.instences.splice(i);
-        return;
-      }
-      callback(entity, type.id);
-    }
-  })
-}
+//Create the game
+let Game = new (require('./../logic/game'))(0, 0);
 
 class Entity {
   constructor({
@@ -43,9 +14,8 @@ class Entity {
     bounds,
     tick = () => {},
     triggers = {},
-    options = {
-      default: true,
-    },
+    collisionSpace,
+    vars = {}
   }){
     this.spriteSheet = spriteSheet;
     this.bounds = bounds;
@@ -55,12 +25,9 @@ class Entity {
 
     this.instences = [];
 
-    this.options = options;
-    if(this.options !== undefined){
-      if(this.options.default === true){
-        entityTypes.push(this);
-      }
-    }
+    this.vars = vars;
+
+    Game.entityTypes.push(this);
 
     uID.getID()
     .then((id) => {
@@ -87,21 +54,14 @@ class Instence {
 
     this.colliding = false;
 
-    if(this.parent.options !== undefined){
-      if(this.parent.options.default === true){
-        entitys.push(this);
-      }
-      if(this.parent.options.vars !== undefined){
-        for(let i in this.parent.options.vars){
-          this[i] = this.parent.options.vars[i];
-        }
-      }
+    for(let i in this.parent.vars){
+      this[i] = this.parent.vars[i];
     }
 
     uID.getID()
     .then((id) => {
       this.id = id;
-      collisionSpace.add(this);
+      Game.collisionSpace.add(this);
     });
   }
 
@@ -112,7 +72,7 @@ class Instence {
   tick(delta){
     //If a collider is defiend then we need to do prossessing on it
     if(this.parent.triggers.collision || this.parent.triggers.collisionStart || this.parent.triggers.collisionWhile || this.parent.triggers.collisionEnd){
-      let collision = collisionSpace.testCollision(this);
+      let collision = Game.collisionSpace.testCollision(this);
       if(collision){
         if(this.parent.triggers.collision !== undefined){
           this.parent.triggers.collision.bind(this)();
@@ -152,4 +112,5 @@ class Instence {
   }
 }
 
-module.exports = Entity;
+exports.Entity = Entity;
+exports.Game = Game;
