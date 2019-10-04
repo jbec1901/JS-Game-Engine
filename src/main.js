5c815c5740b78807a6fab64fcacbc753a4c3f27a
Leyla <leyla@pop-os.localdomain>
2019-10-03 23:30:50 -0500

    Started to recreate game

--- src/main.js
+++ src/main.js
@@ -3,25 +3,74 @@ const Entity = require('./physics/entity');
 const AABB = require('./physics/collision/AABB');
 const Vector = require('./physics/vector');
 const SpriteSheet = require('./IO/spriteSheet');
+const UserInput = require('./IO/userInput');
 
-class Box {
-  constructor(pos, vel){
-    this.position = pos;
-    this.velocity = vel;
+class Player {
+  constructor(high, low){
+
+    this.heightError = 5;
+
+    this.high = high;
+    this.low = low;
+
+    this.radius = low;
+    this.targetHeight = false;
+
+    this.speed = 0;
+    this.rotation = 0;
 
     this.collider = new AABB(new Vector(16));
-    this.spriteSheet = new SpriteSheet('./../res/red.png', 16, 16);
+    this.spriteSheet = new SpriteSheet('./../res/green.png', 16, 16);
 
     Entity.call(this);
   }
 
+  tick(delta){
+    if(this.radius >= this.high){
+      this.radius = this.high;
+    }
+    else if(this.radius <= this.low){
+      this.radius = this.low;
+    }
+
+    if(this.targetHeight && this.radius <= this.high - this.heightError){
+      this.radius += delta * 2;
+    }
+    else if(!this.targetHeight && this.radius >= this.low + this.heightError){
+      this.radius += -delta * 2;
+    }
+
+    this.rotation += (this.speed / (this.radius * 2 * Math.PI) * delta);
+
+    this.position = new Vector(Math.sin(this.rotation) * this.radius + 680, Math.cos(this.rotation) * this.radius + this.high + 50);
+  }
+
   collisionStart(){
-    console.log('test');
+    this.exist = false;
   }
 }
+let player = new Player(280, 100);
 
-new Box(new Vector(), new Vector(1,0));
-new Box(new Vector(100, 0), new Vector());
+UserInput.Controler.addBinding('KeyW', 'switch');
+UserInput.Controler.addBinding('KeyS', 'switch');
+UserInput.Controler.down('switch', () => {
+  player.targetHeight = !player.targetHeight;
+});
+UserInput.Controler.addBinding('KeyA', 'left');
+UserInput.Controler.down('left', () => {
+  player.speed = -5;
+});
+UserInput.Controler.up('left', () => {
+  player.speed = 0;
+});
+
+UserInput.Controler.addBinding('KeyD', 'right');
+UserInput.Controler.down('right', () => {
+  player.speed = 5;
+});
+UserInput.Controler.up('right', () => {
+  player.speed = 0;
+});
 
 Game.start({
   loop: () => { },
